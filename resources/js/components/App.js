439339169810e82afd4d4e7b354daa125cb34c9e
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2020-03-08 06:43:35 +0700

    initial commit

---
+++ resources/js/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header'
+import ProjectsList from './ProjectsList'
+import NewProject from './NewProject'
+import SingleProject from './SingleProject'
+
+class App extends Component {
+    render () {
+        return (
+        <BrowserRouter>
+            <div>
+            <Header />
+            <Switch>
+                <Route exact path='/' component={ProjectsList} />
+                <Route path='/create' component={NewProject} />
+                <Route path='/:id' component={SingleProject} />
+            </Switch>
+            </div>
+        </BrowserRouter>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
